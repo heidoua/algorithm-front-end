54727fba3258444e6345268e274de8cb9b7293ea
heidou <294925572@qq.com>
2024-11-26 14:39:24 +0800

    反转字符串

--- 3.字符串/1.reverseStr.js
+++ 3.字符串/1.reverseStr.js
@@ -1,6 +1,20 @@
-const str = '123'
+const str = '1234567'
 console.log(reverseStr(str))
 
 function reverseStr(str) {
   return str.split('').reverse().join('')
+}
+
+function reverseStr(str) {
+  let newStr = ''
+  
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i]
+  }
+
+  return newStr
+}
+
+function reverseStr(str) {
+  return str.split('').reduceRight((pre, cur) => pre + cur)
 }
